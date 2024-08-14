0355f083b2451e9f4590076e37a89a91c055356a
Leonardo Gurgel <leonardogurgelmf@gmail.com>
2024-08-13 21:53:47 -0300

    improve summary rerendering

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,28 +1,59 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import type { PropsWithChildren, ReactElement } from "react";
+import {
+	ArrowCircleDown,
+	ArrowCircleUp,
+	CurrencyDollar,
+	type IconProps,
+} from "phosphor-react";
+import type {
+	ForwardRefExoticComponent,
+	PropsWithChildren,
+	RefAttributes,
+} from "react";
+import { useTransactions } from "../hooks/useTransactions";
 import { cn } from "../utils/cn";
+import { priceFormatter } from "../utils/formatter";
 
 export function Summary() {
+	const { data, error, isPending } = useTransactions("");
+
+	if (error != null) {
+		return <></>;
+	}
+
+	const incomes = isPending
+		? 0
+		: data
+				.filter((t) => t.type === "income")
+				.reduce((acc, t) => acc + t.price, 0);
+	const expenses = isPending
+		? 0
+		: data
+				.filter((t) => t.type === "expense")
+				.reduce((acc, t) => acc + t.price, 0);
+
 	return (
 		<section className="w-full max-w-6xl mx-auto px-6 grid grid-cols-3 gap-8 -mt-20">
 			<SummaryCard
 				title="Entradas"
-				icon={<ArrowCircleUp className="text-emerald-600" size={32} />}
+				Icon={ArrowCircleUp}
+				iconColor="text-emerald-600"
 			>
-				R$ 17.400,00
+				{priceFormatter.format(incomes)}
 			</SummaryCard>
 			<SummaryCard
 				title="Saídas"
-				icon={<ArrowCircleDown className="text-rose-800" size={32} />}
+				Icon={ArrowCircleDown}
+				iconColor="text-rose-800"
 			>
-				R$ 17.400,00
+				{priceFormatter.format(expenses)}
 			</SummaryCard>
 			<SummaryCard
 				title="Total"
 				className="bg-emerald-700"
-				icon={<CurrencyDollar className="text-gray-100" size={32} />}
+				iconColor="text-gray-100"
+				Icon={CurrencyDollar}
 			>
-				R$ 17.400,00
+				{priceFormatter.format(incomes - expenses)}
 			</SummaryCard>
 		</section>
 	);
@@ -30,16 +61,24 @@ export function Summary() {
 
 interface SummaryCardProps extends PropsWithChildren {
 	title: string;
-	icon: ReactElement;
+	// biome-ignore lint/style/useNamingConvention: Icon is a component
+	Icon: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>;
+	iconColor: string;
 	className?: string;
 }
 
-function SummaryCard({ title, icon, children, className }: SummaryCardProps) {
+function SummaryCard({
+	title,
+	Icon,
+	iconColor,
+	children,
+	className,
+}: SummaryCardProps) {
 	return (
 		<div className={cn("bg-gray-700 rounded-md p-8", className)}>
 			<header className="flex items-center justify-between text-gray-400">
 				<span>{title}</span>
-				{icon}
+				<Icon size={32} className={iconColor} />
 			</header>
 			<strong className="block mt-4 text-3xl">{children}</strong>
 		</div>
